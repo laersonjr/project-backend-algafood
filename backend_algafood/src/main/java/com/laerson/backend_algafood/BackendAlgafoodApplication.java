96eba8b3d38fced29b059f015c429e894308343d
laersonjr <laersonjr@gmail.com>
2023-09-28 23:01:32 -0300

    :tada: Commit inicial

---
+++ backend_algafood/src/main/java/com/laerson/backend_algafood/BackendAlgafoodApplication.java
@@ -0,0 +1,13 @@
+package com.laerson.backend_algafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAlgafoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendAlgafoodApplication.class, args);
+    }
+
+}
---
+++ backend_algafood/src/test/java/com/laerson/backend_algafood/BackendAlgafoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laerson.backend_algafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAlgafoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
